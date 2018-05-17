d8fa89c41f3d54b7828649da4b773093f9105c09
gitogitton <mukyu_02@yahoo.co.jp>
2018-05-17 16:29:36 +0900

    サービスクラス追加。 Service, IntentService から継承クラス。 理解しきれてない「とりあえず版」。処理も出来てません。

--- app/src/main/java/myrelrec/myappl/jp/mytelrec/FragmentMain.java
+++ app/src/main/java/myrelrec/myappl/jp/mytelrec/FragmentMain.java
@@ -1,6 +1,10 @@
 package myrelrec.myappl.jp.mytelrec;
 
+import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -18,6 +22,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 
@@ -27,7 +32,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 public class FragmentMain extends Fragment {
 
@@ -43,7 +48,10 @@ public class FragmentMain extends Fragment {
 
     private Context mContext;
     private View mView;
+    private SwitchCompat mSwitchCompat;
     private SettingData mSettingData = new SettingData();
+
+    private MediaPlayer mMediaPlayer = null;
 
     public FragmentMain() {
         // Required empty public constructor
@@ -65,6 +73,7 @@ public class FragmentMain extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        mMediaPlayer = new MediaPlayer();
     }
 
     @Override
@@ -83,6 +92,9 @@ public class FragmentMain extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         showRecordingFileList();
         restoreSettingData();
+        if ( isRecordServiceAlive() ) { //通話録音サービスが起動中なら
+            mSettingData.setAutoStart( true ); //通話と同時に録音を開始に設定
+        }
     }
 
     @Override
@@ -98,10 +110,16 @@ public class FragmentMain extends Fragment {
 
         MenuItem item = menu.findItem( R.id.menu_activate );
         SwitchCompat switchCompat = (SwitchCompat) item.getActionView();
+        switchCompat.setChecked( mSettingData.isAutoStart() );
         switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Log.d( LOG_TAG, "onCheckedChanged()" );
+                if ( isChecked ) {
+                    startTelRecService();
+                } else {
+                    stopTelRecService();
+                }
             }
         });
     }
@@ -131,41 +149,131 @@ public class FragmentMain extends Fragment {
     //
     //private methods
     //
+    private boolean isRecordServiceAlive() {
+        //（確か）API 26から廃止
+        ActivityManager manager = (ActivityManager) mContext.getSystemService( Context.ACTIVITY_SERVICE );
+        for ( ActivityManager.RunningServiceInfo serviceInfo : manager.getRunningServices(Integer.MAX_VALUE ) ) {
+            if ( TelRecService.class.getName().equals( serviceInfo.service.getClassName() ) ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void startTelRecService() {
+
+        //
+        //サービス開始処理
+        //　 開始出来た場合や既にある場合はその情報が返ってくる。
+        //   起動失敗すると null で返ってくる。（と以下の英文を読んだからサービスが既に存在するか否かのチェックはいらない！！！、、と思った。）
+        //
+
+        // If the service is being started or is already running, the ComponentName of the actual service that was started is returned;
+        // else if the service does not exist null is returned. (wrote by google site)
+//        Intent intent = new Intent( mContext, TelRecIntentService.class );
+        Intent intent = new Intent( mContext, TelRecService.class );
+        ComponentName componentName = mContext.startService( intent );
+        if ( componentName == null ) {
+            Log.d( LOG_TAG, "Service doesn't exist." );
+            return;
+        }
+
+        Log.d( LOG_TAG, "Service is started." );
+    }
+
+    private void stopTelRecService() {
+
+        //
+        //サービス終了処理
+        //
+
+//        Intent intent = new Intent( mContext, TelRecIntentService.class );
+        Intent intent = new Intent( mContext, TelRecService.class );
+        // If there is a service matching the given Intent that is already running,
+        // then it is stopped and true is returned;
+        // else false is returned.  (wrote by google site)
+        boolean result = mContext.stopService( intent );
+        if ( !result ) {
+            Log.d( LOG_TAG, "Service is not found." );
+            return;
+        }
+
+        Log.d( LOG_TAG, "Service is stopped." );
+    }
+
+    private boolean isServiceStarted() {
+        ActivityManager manager = (ActivityManager) mContext.getSystemService( Context.ACTIVITY_SERVICE );
+
+        //
+        // ActivityManager#getRunningServices()
+        // This method was deprecated in API level 26.
+        // ( use bellow)
+        //
+
+        if ( manager == null ) {
+            return false;
+        }
+        List<ActivityManager.RunningServiceInfo> info = manager.getRunningServices( Integer.MAX_VALUE );
+        for ( ActivityManager.RunningServiceInfo serviceInfo : manager.getRunningServices( Integer.MAX_VALUE ) ) {
+//            if ( YourService.class.getName().equals( serviceInfo.service.getClassName() ) ) {
+                return true;
+//            }
+        }
+        return false;
+    }
+
     private void showRecordingFileList() {
         ArrayList<ItemData> arrayList = getFileList();
         RecordingFileListAdapter adapter = new RecordingFileListAdapter( mContext, R.layout.file_list_item, arrayList );
 
         ListView listView = mView.findViewById( R.id.list_recordFileList );
         listView.setAdapter( adapter );
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Log.d( LOG_TAG, "select->" + position );
+                ItemData item = (ItemData) parent.getItemAtPosition( position );
+                Log.d( LOG_TAG, "item->"+item.getDate()+" / "+item.getPhoneNumber() );
+
+                //
+                //再生処理を書こう！！！
+                //
+                //
+                // ↓　とりあえずの処理なのであとでちゃんと作れ！！
+                if ( mMediaPlayer != null && mMediaPlayer.isPlaying() ) {
+                    Log.d( LOG_TAG, "mediaPlayter stop." );
+                    mMediaPlayer.pause();
+                    mMediaPlayer.stop();
+                    mMediaPlayer.reset();
+                } else {
+                    Log.d( LOG_TAG, "mediaPlayter start." );
+                    mMediaPlayer = MediaPlayer.create( mContext, Uri.parse( "/storage/sdcard0/telrec/"+item.getPhoneNumber() ) );
+                    mMediaPlayer.start();
+                    mMediaPlayer.setVolume( (float) 1.0, (float)1.0 ); // 0.0 - 1.0
+                    mMediaPlayer.setLooping( false );
+                }
+//                mMediaPlayer.setVolume(    );
+            }
+        });
     }
 
     private ArrayList<ItemData> getFileList() {
         ArrayList<ItemData> arrayList = new ArrayList<>();
-//test data start
-        ItemData[] itemData = new ItemData[10];
-        for ( int i=0; i< itemData.length; i++ ) {
-            itemData[ i ] = new ItemData();
-            String stringDate;
 
-//            Log.d( LOG_TAG, " i -> " + i );
-//            if ( i!=0 && ( i%3 == 0 ) ) {
-//                stringDate = "";
-//            } else {
-//                stringDate = "data" + Integer.toString( i );
-//            }
-
-            stringDate = "data" + Integer.toString( i );
-
-            String stringNumber = "name" + Integer.toString( i );
-            itemData[ i ].setDate( stringDate );
-            itemData[ i ].setPhoneNumber( stringNumber );
+//        File fileList = new File( mContext.getFilesDir() + "/telrec/" );
+        File fileList = new File( "/storage/sdcard0/telrec/" );
+        String[] dirList = fileList.list();
+        if ( dirList == null || dirList.length <= 0 ) {
+            return null;
         }
-//test data end
 
-//        for ( ItemData item : itemData ) {
-//            arrayList.add( item );
-//        }
-        arrayList.addAll( Arrays.asList(itemData) );
+        for ( String item : dirList ) {
+            Log.d( LOG_TAG, "file->" + item );
+            ItemData itemData = new ItemData();
+            itemData.setDate( "2018/5/15" );
+            itemData.setPhoneNumber( item );
+            arrayList.add( itemData );
+        }
 
         return arrayList;
     }
--- app/src/main/java/myrelrec/myappl/jp/mytelrec/MainActivity.java
+++ app/src/main/java/myrelrec/myappl/jp/mytelrec/MainActivity.java
@@ -1,17 +1,10 @@
 package myrelrec.myappl.jp.mytelrec;
 
 import android.support.v4.app.FragmentManager;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
-import static android.app.PendingIntent.FLAG_ONE_SHOT;
 
 public class MainActivity extends AppCompatActivity implements FragmentSettingForm.OnFinishSettingFormListener {
 
@@ -36,39 +29,5 @@ public class MainActivity extends AppCompatActivity implements FragmentSettingFo
     @Override
     public void OnFinishSettingForm() {
         Log.d( LOG_TAG, "OnFinishSettingFormListener() start." );
-    }
-
-    //
-    //private methods
-    //
-    private void setNotification( boolean state ) {
-        if ( state ) {
-
-            //通知をクリックした時起動するアプリ設定
-            Intent notifyIntent = new Intent();
-            notifyIntent.setClass( this, MainActivity.class );
-            PendingIntent pendingIntent = PendingIntent.getActivity( this, 0, notifyIntent, FLAG_ONE_SHOT );
-            //通知を発行するmanager
-            NotificationManager nManager = (NotificationManager) getSystemService( Context.NOTIFICATION_SERVICE );
-            //通知の設定
-            Notification.Builder builder = new Notification.Builder( this );
-            builder.setSmallIcon( R.mipmap.ic_launcher_round );
-            builder.setContentTitle( "通話記録中" );
-            builder.setContentText( "コンテンツテキストです" );
-            builder.setContentIntent( pendingIntent );
-            if ( nManager != null ) { nManager.notify( 1/*ID*/, builder.build() ); }
-            //
-            //通話録音を開始する。
-            //
-
-        } else {
-            NotificationManager nManager = (NotificationManager) getSystemService( Context.NOTIFICATION_SERVICE );
-            if ( nManager != null ) { nManager.cancel( 1 ); }
-
-            //
-            //通話録音を終了する。
-            //
-            Log.d( LOG_TAG, "" );
-        }
     }
 }
