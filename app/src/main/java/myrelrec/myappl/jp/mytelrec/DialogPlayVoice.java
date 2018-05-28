175f53440fa1e06465403e9cfda069ec9d22fb1f
gitogitton <mukyu_02@yahoo.co.jp>
2018-05-28 14:45:35 +0900

    開発環境を変更したのでUpする。

--- app/src/main/java/myrelrec/myappl/jp/mytelrec/DialogPlayVoice.java
+++ app/src/main/java/myrelrec/myappl/jp/mytelrec/DialogPlayVoice.java
@@ -2,13 +2,17 @@ package myrelrec.myappl.jp.mytelrec;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.SeekBar;
 
 import java.util.Objects;
 
@@ -18,6 +22,9 @@ public class DialogPlayVoice extends DialogFragment {
     private String mDialogTitle = "音声を再生";
     private String mMessage = "";
     private View mView;
+    private Context mContext;
+    private int mLastProgress = -1; //SeekBarの位置
+    private int mLastPos = -1;
 
     @NonNull
     @Override
@@ -53,7 +60,88 @@ public class DialogPlayVoice extends DialogFragment {
             }
         });
 
+        initSeekBar();
+
+        setListeners();
+
         return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    //
+    // private methods
+    //
+    private void initSeekBar() {
+        //再生位置のseekBarを現在の値に合わせる。
+        SeekBar seekBarPlayPos = mView.findViewById( R.id.seekBar_play_position );
+
+        //ボリュームのseekBarを現在の値に合わせる。
+        SeekBar seekBarVolume = mView.findViewById( R.id.seekBar_volume );
+        AudioManager audioManager = (AudioManager) mContext.getSystemService( Context.AUDIO_SERVICE );
+        int volume = audioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
+        int volumeMax = audioManager.getStreamMaxVolume( AudioManager.STREAM_MUSIC );
+        int progressValue = (int)( ( (float)volume/(float)volumeMax) * (float)100.0 );
+        seekBarVolume.setProgress( progressValue );
+        //Log.d( LOG_TAG, "initSeekBar() currentVol/maxVol/progressVal->" + volume + " / " + volumeMax + " / " + progressValue );
+    }
+
+    private void setListeners() {
+
+        SeekBar seekBarPlayPos = mView.findViewById( R.id.seekBar_play_position );
+        final SeekBar seekBarVolume = mView.findViewById( R.id.seekBar_volume );
+
+        seekBarVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d( LOG_TAG, "seekBarVolume.onProgressChanged() progress/fromUser ->" + progress + " / " + fromUser );
+                mLastProgress = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d( LOG_TAG, "seekBarVolume.onStartTrackingTouch()" );
+                // for debug
+                AudioManager audioManager = (AudioManager) mContext.getSystemService( Context.AUDIO_SERVICE );
+                int volume = audioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
+                Log.d( LOG_TAG, "seekBarVolume.onStartTrackingTouch() Volume : current->" + volume );
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                AudioManager audioManager = (AudioManager) mContext.getSystemService( Context.AUDIO_SERVICE );
+                int volume = audioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
+                int volumeMax = audioManager.getStreamMaxVolume( AudioManager.STREAM_MUSIC );
+                int setVolumeValue = (int)( (float)volumeMax * (float)mLastProgress/(float)seekBarVolume.getMax() );
+                audioManager.setStreamVolume( AudioManager.STREAM_MUSIC, setVolumeValue, AudioManager.FLAG_SHOW_UI ); // デバッグ終わると変更 : third argument -> AudioManager.FLAG_VIBRATE
+
+//                Log.d( LOG_TAG, "seekBarVolume.onStopTrackingTouch() Volume : getMax() / lastProgress->" + seekBarVolume.getMax() + " / " + mLastProgress );
+//                Log.d( LOG_TAG, "seekBarVolume.onStopTrackingTouch() Volume : current/max->" + volume + " / " + volumeMax );
+//                Log.d( LOG_TAG, "seekBarVolume.onStopTrackingTouch() Volume : setVolumeValue->" + setVolumeValue );
+            }
+        });
+
+        seekBarPlayPos.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d( LOG_TAG, "seekBarPlayPos.onProgressChanged() progress/fromUser ->" + progress + " / " + fromUser );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d( LOG_TAG, "seekBarPlayPos.onStartTrackingTouch()" );
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d( LOG_TAG, "seekBarPlayPos.onStopTrackingTouch()" );
+            }
+        });
+
     }
 
 }
